7e7e2eb03b910dd49cea0cee6a84418621dd845a
Viet Phan <viet.phanho@asnet.com.vn>
2019-10-22 14:06:20 +0700

    Add UnitTest1

---
+++ simple-web-api-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace simple_web_api_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.NotEqual(5, 4);
+        }
+    }
+}
